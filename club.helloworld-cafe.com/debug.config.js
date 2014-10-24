e22f7164540029eb0cc8cbeb8a5cf36653797012
liqinglin0701 <shenwuliebo@163.com>
2014-10-24 18:46:38 +0800

    #22 #23 update icon and admins

--- club.helloworld-cafe.com/debug.config.js
+++ club.helloworld-cafe.com/debug.config.js
@@ -20,8 +20,8 @@ var config = {
   site_headers: [
     '<meta name="author" content="hailiang.hl.wang@gmail.com" />'
   ],
-  site_logo: '/public/images/cnodejs_light.svg', // default is `name`
-  site_icon: '/public/images/cnode_icon_32.png', // 默认没有 favicon, 这里填写网址
+  site_logo: '/public/images/logo.png', // default is `name`
+  site_icon: '/public/images/logo.ico', // 默认没有 favicon, 这里填写网址
   // 右上角的导航区
   site_navs: [
     // 格式 [ path, title, [target=''] ]
@@ -76,7 +76,9 @@ var config = {
   weibo_id: 'your_weibo_id',
 
   // admin 可删除话题，编辑标签，设某人为达人
-  admins: { hain9: true },
+  admins: { hain9: true ,
+    kalas: true,
+    sally: true},
 
   // github 登陆的配置
   GITHUB_OAUTH: {
